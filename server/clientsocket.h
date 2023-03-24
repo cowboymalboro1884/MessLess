6fd6386cf08abc0cf76762e834ded6ca9f09da5c
cowboymalboro1884 <112244451+cowboymalboro1884@users.noreply.github.com>
2023-03-24 19:15:30 +0300

    adding processing "project"-requests

---
+++ server/clientsocket.h
@@ -0,0 +1,42 @@
+#ifndef CLIENTSOCKET_H
+#define CLIENTSOCKET_H
+
+#include <QDebug>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QString>
+#include <QTcpSocket>
+
+#include "database.h"
+#include "requestholder.h"
+
+class Server;
+
+class ClientSocket : QObject {
+    Q_OBJECT
+public:
+    explicit ClientSocket(qintptr ID, Server *server_, QTcpSocket* socket_, messless::Database *db_, QObject *parent = nullptr);
+    ~ClientSocket();
+    void sendData(const QByteArray& data);
+    qint16 get_id() const;
+    QTcpSocket *socket;
+    bool isCheckSuccessfull = false;
+
+    RequestHolder* holder;
+
+    QString user_role;
+    QString email;
+    QString password;
+
+public slots:
+    void readData();
+    void disconnected();
+
+private:
+    messless::Database* db;
+    Server *server;
+    qintptr socket_descriptor;
+};
+
+
+#endif // CLIENTSOCKET_H
---
+++ server/server.cpp
@@ -0,0 +1,67 @@
+#include "server.h"
+
+Server::Server(QObject *parent)
+    : QObject(parent){
+
+      };
+
+bool Server::startServer(qint16 port, std::string &config_file) {
+  server = new QTcpServer(this);
+  PORT = port;
+
+  if (server->listen(QHostAddress::Any, PORT)) {
+    qDebug() << "Listening port" << PORT << "...";
+    qDebug() << "My ip " << server->serverAddress().toString();
+
+    connect(server, SIGNAL(newConnection()), this, SLOT(incomingConnection()));
+
+    try {
+      connectToDatabase(config_file);
+      qDebug() << "Connected to database";
+    } catch (...) {
+      qDebug() << "Couldn't connect to database";
+      return false;
+    }
+  } else {
+    qDebug() << "Couldn't start server";
+    return false;
+  }
+  return true;
+}
+
+void Server::incomingConnection() {
+  qDebug() << "New connection!";
+  QTcpSocket *next_connection = server->nextPendingConnection();
+  ClientSocket *connected_socket = new ClientSocket(
+      next_connection->socketDescriptor(), this, next_connection, db);
+  sockets.push_back(connected_socket);
+}
+
+void Server::sockDisc(ClientSocket *socket) {
+  qDebug() << "Disconnected" << socket->get_id();
+
+  for (int i = 0; i < sockets.size(); i++) {
+    if (socket == sockets[i]) {
+      delete sockets[i];
+      sockets.removeAt(i);
+      break;
+    }
+  }
+}
+
+void Server::connectToDatabase(std::string &config_file) {
+  std::ifstream input(config_file);
+  std::string private_salt, connection_string;
+  std::getline(input, connection_string);
+  std::getline(input, private_salt);
+  messless::Database db_(connection_string, private_salt);
+  db = &db_;
+}
+
+Server::~Server() {
+  for (auto &client : sockets) {
+    client->socket->deleteLater();
+  }
+  server->deleteLater();
+  delete db;
+}
