4f843fdeff3105cfb2732fc66acdf7ea228942c5
cowboymalboro1884 <112244451+cowboymalboro1884@users.noreply.github.com>
2023-04-06 01:25:36 +0300

    Update clientsocket.h

--- server/clientsocket.h
+++ server/clientsocket.h
@@ -13,30 +13,30 @@
 class Server;
 
 class ClientSocket : QObject {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit ClientSocket(qintptr ID, Server *server_, QTcpSocket* socket_, messless::Database *db_, QObject *parent = nullptr);
-    ~ClientSocket();
-    void sendData(const QByteArray& data);
-    qint16 get_id() const;
-    QTcpSocket *socket;
-    bool isCheckSuccessfull = false;
+  explicit ClientSocket(qintptr ID, Server *server_, QTcpSocket *socket_,
+                        messless::Database *db_, QObject *parent = nullptr);
+  ~ClientSocket();
+  void sendData(const QByteArray &data);
+  qint16 get_id() const;
+  QTcpSocket *socket;
+  bool isCheckSuccessfull = false;
 
-    RequestHolder* holder;
+  RequestHolder *holder;
 
-    QString user_role;
-    QString email;
-    QString password;
+  QString user_role;
+  QString email;
+  QString password;
 
 public slots:
-    void readData();
-    void disconnected();
+  void readData();
+  void disconnected();
 
 private:
-    messless::Database* db;
-    Server *server;
-    qintptr socket_descriptor;
+  messless::Database *db;
+  Server *server;
+  qintptr socket_descriptor;
 };
-
 
 #endif // CLIENTSOCKET_H
