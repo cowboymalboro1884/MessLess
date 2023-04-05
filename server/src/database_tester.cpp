2f940c25013c7ab8fb738afabd0b37bac136ba19
Andrew Bolotsky <andrew04tlt@gmail.com>
2023-04-05 12:37:28 +0300

    исправление мелких ошибок, проверка работоспособности реализации

--- server/src/database_tester.cpp
+++ server/src/database_tester.cpp
@@ -44,8 +44,9 @@ int main() {
     std::cout<< messless::DatabaseProject::create_project(db,user_info1,"my_project","")<<"Creating project\n";
     std::cout<< messless::DatabaseProject::get_project_id(db,user_info1,"my_project")<<"Project id\n";
     unsigned int prID =messless::DatabaseProject::get_project_id(db,user_info1,"my_project");
-    std::cout<<"PROJECT ID GOT";
+    std::cout<<"PROJECT ID GOT\n";
     messless::PrivateUserInfo user_info2 =messless::DatabaseUser::login_user(db,"amogus11@gmail.com","quwuqewq!!!");
-    messless::DatabaseProject::add_user_in_project(db,user_info2,prID,"admin");
+    std::cout<<"LOGIN\n";
+    //messless::DatabaseProject::add_user_in_project(db,user_info2,prID,"moderator");
     std::cout<<"Add user to project\n";
 }
