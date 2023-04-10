5e20bca5b9a0e12cabfe8510980607f149c26f2d
Andrew Bolotsky <andrew04tlt@gmail.com>
2023-04-10 14:37:20 +0300

    исправление мелких ошибок

--- server/src/database_tester.cpp
+++ server/src/database_tester.cpp
@@ -41,12 +41,20 @@ int main() {
                  )
                      .user_role
               << "User login\n";*/
-    messless::PrivateUserInfo user_info1 =messless::DatabaseUser::login_user(db, "amogus@gmail.com", "quwuqewq!!!");
+    /*messless::PrivateUserInfo user_info1 =messless::DatabaseUser::login_user(db, "amogus@gmail.com", "quwuqewq!!!");
     std::cout<< messless::DatabaseProject::create_project(db,user_info1,"my_project","")<<"Creating project\n";
     std::cout<< messless::DatabaseProject::get_project_id(db,user_info1,"my_project")<<"Project id\n";
     unsigned int prID =messless::DatabaseProject::get_project_id(db,user_info1,"my_project");
     messless::PrivateUserInfo user_info2 =messless::DatabaseUser::login_user(db,"amogus11@gmail.com","zalupa");
-    messless::DatabaseProject::add_user_in_project(db,user_info2,prID,"moderator");
+    messless::DatabaseProject::add_user_in_project(db,user_info2,prID,"moderator");*/
+    messless::DatabaseCompany::create_user(
+        db, "amogus34@gmail.com", "quwuqewq!!!", "Vladimir", "EEEE",
+        1, "employee"
+    );
+    messless::PrivateUserInfo user_info2 =messless::DatabaseUser::login_user(db,"amogus34@gmail.com","quwuqewq!!!");
+    messless::PrivateUserInfo user_info1 =messless::DatabaseUser::login_user(db, "amogus@gmail.com", "quwuqewq!!!");
+    unsigned int prID =messless::DatabaseProject::get_project_id(db,user_info1,"my_project");
+    messless::DatabaseProject::add_user_in_project(db,user_info2,prID,"employee");
     std::vector<messless::User> project1_users = messless::DatabaseProject::get_project_user_list(db,prID);
     for (auto &x:project1_users){
         std::cout<<x.name<<" "<<x.surname<<" "<<x.email<<" "<<x.user_role<<'\n';
