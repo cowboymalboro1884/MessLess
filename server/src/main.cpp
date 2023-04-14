d3d9f77d77f2f717987d5ae4a9290bc3302d97be
cowboymalboro1884 <112244451+cowboymalboro1884@users.noreply.github.com>
2023-04-14 17:50:25 +0300

    Update main.cpp

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    qint16 port = 1338;
+    qint16 port = 1339;
     std::string config_file = "/root/database.config";
 
-    if (!Server::getInstance().startServer(port, config_file)) {
+    if (!Server::get_instance().start_server(port, config_file)) {
         return 0;
     };
 
