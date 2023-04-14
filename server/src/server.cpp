afb60efe30e032234da4becb35d5201634c0c640
cowboymalboro1884 <112244451+cowboymalboro1884@users.noreply.github.com>
2023-04-14 17:47:54 +0300

    Update server.cpp

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -5,57 +5,60 @@ Server::Server(QObject *parent)
 
       };
 
-bool Server::startServer(qint16 port, std::string &config_file) {
-  server = new QTcpServer(this);
-  PORT = port;
+bool Server::start_server(qint16 port, std::string &config_file) {
+    server = new QTcpServer(this);
+    PORT = port;
 
-  if (server->listen(QHostAddress::Any, PORT)) {
-    qDebug() << "Listening port" << PORT << "...";
-    qDebug() << "My ip " << server->serverAddress().toString();
+    if (server->listen(QHostAddress::Any, PORT)) {
+        qDebug() << "Listening port" << PORT << "...";
+        qDebug() << "My ip " << server->serverAddress().toString();
 
-    connect(server, SIGNAL(newConnection()), this, SLOT(incomingConnection()));
+        connect(
+            server, SIGNAL(newConnection()), this, SLOT(incoming_connection())
+        );
 
-    if (connectToDatabase(config_file)) {
-      qDebug() << "Connected to database";
+        if (connect_to_database(config_file)) {
+            qDebug() << "Connected to database";
+        } else {
+            qDebug() << "Couldn't connect to database";
+            return false;
+        }
     } else {
-      qDebug() << "Couldn't connect to database";
-      return false;
+        qDebug() << "Couldn't start server";
+        return false;
     }
-  } else {
-    qDebug() << "Couldn't start server";
-    return false;
-  }
-  return true;
+    return true;
 }
 
-void Server::incomingConnection() {
-  qDebug() << "New connection!";
-  QTcpSocket *next_connection = server->nextPendingConnection();
-  qDebug() << db->connection.is_open() << "connection\n";
-  ClientSocket *connected_socket = new ClientSocket(
-      next_connection->socketDescriptor(), this, next_connection, db);
-  sockets[connected_socket->get_id()] = connected_socket;
+void Server::incoming_connection() {
+    qDebug() << "New connection!";
+    QTcpSocket *next_connection = server->nextPendingConnection();
+    qDebug() << m_db->connection.is_open() << "connection\n";
+    ClientSocket *connected_socket = new ClientSocket(
+        next_connection->socketDescriptor(), this, next_connection, m_db
+    );
+    connected_sockets[connected_socket->get_id()] = connected_socket;
 }
 
-void Server::sockDisc(ClientSocket *socket) {
-  qDebug() << "Disconnected" << socket->get_id();
-  sockets.erase(sockets.find(socket->get_id()));
+void Server::sock_disc(ClientSocket *socket) {
+    connected_sockets.erase(connected_sockets.find(socket->get_id()));
+    delete socket;
 }
 
-bool Server::connectToDatabase(std::string &config_file) {
-  std::ifstream input(config_file);
-  std::string private_salt, connection_string;
-  std::getline(input, connection_string);
-  std::getline(input, private_salt);
-  static messless::Database db_(connection_string, private_salt);
-  db = &db_;
-  return db_.connection.is_open();
+bool Server::connect_to_database(std::string &config_file) {
+    std::ifstream input(config_file);
+    std::string private_salt, connection_string;
+    std::getline(input, connection_string);
+    std::getline(input, private_salt);
+    static messless::Database db_(connection_string, private_salt);
+    m_db = &db_;
+    return db_.connection.is_open();
 }
 
 Server::~Server() {
-  for (auto &client : sockets) {
-    client->socket->deleteLater();
-  }
-  server->deleteLater();
-  delete db;
+    for (auto &client : connected_sockets) {
+        client->socket->deleteLater();
+    }
+    server->deleteLater();
+    delete m_db;
 }
