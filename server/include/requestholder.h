d887ef64107fb5c4bd107616ee61e7ea5119af9a
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-05-22 00:32:20 +0300

    Add files via upload

---
+++ server/include/requestholder.h
@@ -0,0 +1,33 @@
+#ifndef REQUESTHOLDER_H
+#define REQUESTHOLDER_H
+
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTcpSocket>
+#include <random>
+#include "database.h"
+#include "databaseproject.h"
+#include "databasechats.h"
+
+class ClientSocket;
+
+class RequestHolder {
+    messless::Database *database;
+    ClientSocket *owner;
+    mutable messless::PrivateUserInfo user;
+
+public:
+    RequestHolder(messless::Database *database_);
+    QJsonDocument proccess_data(const QByteArray &incoming_data) const noexcept;
+
+    QJsonDocument validate_user(const QJsonObject &request) const noexcept;
+    QJsonDocument register_company_with_admin(const QJsonObject &request) const noexcept;
+    QJsonDocument create_project(const QJsonObject &request) const noexcept;
+    QJsonDocument create_task(const QJsonObject &request) const noexcept;
+    QJsonDocument get_tasks(const QJsonObject &request) const noexcept;
+    QJsonDocument get_projects(const QJsonObject &request) const noexcept;
+    QJsonDocument send_message_to_company(const QJsonObject &request) const noexcept;
+};
+
+#endif  // REQUESTHOLDER_H
---
+++ server/include/server.h
@@ -0,0 +1,53 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QMap>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QThread>
+#include <QMutex>
+#include <memory>
+#include "clientsocket.h"
+#include "chatmanager.h"
+
+class Server : public QObject {
+    Q_OBJECT
+
+    QMutex client_mutex;
+    QMap<qint32, ClientSocket *> connected_sockets;
+    QMap<qint32, QSet<qint32> > companies;
+    messless::Database *m_db;
+    qint16 PORT;
+
+public:
+    ~Server();
+    bool start_server(qint16 port, std::string &config_file);
+    void sockReady();
+    void sock_disc(ClientSocket *);
+    bool connect_to_database(std::string &config_file);
+    void send_message_to_company(const qint32 id_company, const QJsonDocument& message);
+
+    QMap<qint32, ClientSocket *>& get_clients(); // socket_id --> clientsocket*
+    QMap<qint32, QSet<qint32> >& get_companies();  // company_id --> set of socket_id
+
+    ChatManager *chat_m;
+
+    static Server &get_instance() {
+        static Server server;
+        return server;
+    };
+
+    QTcpServer *server;
+
+public slots:
+    void incoming_connection();
+
+private:
+    Server(QObject *parent = nullptr);
+    Server(const Server &);
+    Server &operator=(const Server &);
+};
+
+#endif  // SERVER_H
