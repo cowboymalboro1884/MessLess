8898a41eb9dc7fac5729953bd7d2b0a312e84fae
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-05-23 14:38:31 +0300

    Update clientsocket.cpp

--- server/src/clientsocket.cpp
+++ server/src/clientsocket.cpp
@@ -41,8 +41,8 @@ void ClientSocket::read_data() {
     if(json_responce.object().value("recipient").toString() == "to company") {
         int company_id = json_responce.object().value("company_id").toString().toInt();
         qDebug() << "sending data to company" << company_id;
-        qDebug() << server->get_companies()[json_responce.object().value("company_id").toString().toInt()].empty();
-        for(auto socket_id : server->get_companies()[json_responce.object().value("company_id").toString().toInt()]){
+        qDebug() << server->get_companies()[json_responce.object().value("company_id").toInt()].empty();
+        for(auto socket_id : server->get_companies()[json_responce.object().value("company_id").toInt()]){
             qDebug() << "socket: " << socket_id;
         }
         server->send_message_to_company(company_id, json_responce);
