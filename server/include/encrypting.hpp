e1bd7536229aeb5bc077d86124da8a28a24ecc64
Andrew Bolotsky <andrew04tlt@gmail.com>
2023-05-23 14:17:23 +0300

    добавлены квалификаторы

--- server/include/encrypting.hpp
+++ server/include/encrypting.hpp
@@ -45,10 +45,10 @@ class Encrypting {
     static std::string get_hash(std::string &str);
 
 public:
-    static std::string get_random_string();
+    [[nodiscard]] static std::string get_random_string();
     Encrypting();
     explicit Encrypting(std::string salt);
-    std::string get_password_hash(
+    [[nodiscard]] std::string get_password_hash(
         const std::string &password,
         const std::string &personal_salt
     );
