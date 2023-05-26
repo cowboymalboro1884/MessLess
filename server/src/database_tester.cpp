98453bd32a7b182ae442346eb06f708a82cb8bad
Andrew Bolotsky <andrew04tlt@gmail.com>
2023-05-26 16:34:10 +0300

    тестирование работы удаления тасок и проектов

--- server/src/database_tester.cpp
+++ server/src/database_tester.cpp
@@ -26,12 +26,11 @@ int main() {
     auto t = messless::DatabaseChats::from_private_user_info_to_user(
         db, user_info1
     );
+    t.user_role = "employee";
     std::cout<<t.surname<<' '<<t.name<<' '<<t.email<<' '<<t.user_role<<'\n';
     unsigned int task_id = messless::DatabaseProject::create_new_task(
         db, new_project, "new task", "bio", "2013-07-21 00:00:00",
-        {messless::DatabaseChats::from_private_user_info_to_user(
-            db, user_info1
-        )}
+        {t}
     );
 
     messless::DatabaseProject::add_user_to_task(
@@ -42,12 +41,12 @@ int main() {
         std::cout<<x.task_name<<'\n';
     }
     std::cout<<'\n';
-    //messless::DatabaseProject::delete_task(db, task_id);
+    messless::DatabaseProject::delete_task(db, task_id);
 
     for (auto x:messless::DatabaseProject::get_tasks(db,new_project)){
         std::cout<<x.task_name<<'\n';
     }
-
+    messless::DatabaseProject::delete_project(db,messless::DatabaseProject::get_project_id(db,user_info1,"new_project"));
     //messless::DatabaseProject::delete_project(db,new_project);
     // unsigned int new_task =
     // messless::DatabaseProject::create_new_task(db,new_project,"do
