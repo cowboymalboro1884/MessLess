0245cc1eaaa5f3e3444ab04de8ccf10ee5eb46e7
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-06-10 17:33:44 +0300

    Update responses_types.hpp

--- server/include/responses_types.hpp
+++ server/include/responses_types.hpp
@@ -31,17 +31,17 @@ enum ResponseTypes {
 
 inline std::map<ResponseTypes, QString> response_types = {
     {ERROR, "error"},
-    {GOT_STATUS_OF_AUTHORIZATION, "got_status_of_authorization"},
-    {GOT_STATUS_OF_REGISTRATION, "got_status_of_registration"},
-    {REGISTRATION_IS_SUCCESS, "registration_is_success"},
+    {GOT_STATUS_OF_AUTHORIZATION, "got status of authorization user"},
+    {GOT_STATUS_OF_REGISTRATION, "got status of registration user and company"},
+    {REGISTRATION_IS_SUCCESS, "registration new user is success"},
 
-    {GOT_PROJECTS_TO_UPDATE, "got_projects_to_update"},
-    {NEW_CONDITION_OF_PROJECTS, "new_condition_of_projects"},
+    {GOT_PROJECTS_TO_UPDATE, "got projects to update"},
+    {NEW_CONDITION_OF_PROJECTS, "new condition of projects"},
 
-    {GOT_TASKS_TO_UPDATE, "got_tasks_to_update"},
-    {NEW_CONDITION_OF_TASKS, "new_condition_of_tasks"},
+    {GOT_TASKS_TO_UPDATE, "got tasks to update"},
+    {NEW_CONDITION_OF_TASKS, "new condition of tasks"},
 
-    {GOT_MESSAGE_TO_COMPANY_CHAT, "got_message_to_company_chat"},
-    {GOT_MESSAGE_TO_PROJECT_CHAT, "got_message_to_project_chat"}};
+    {GOT_MESSAGE_TO_COMPANY_CHAT, "got message to company chat"},
+    {GOT_MESSAGE_TO_PROJECT_CHAT, "got message to project chat"}};
 
 #endif  // RESPONSES_TYPES_H
