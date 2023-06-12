57214eddc9c220be9eae6deabaf8d45806d4ab96
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-06-12 03:05:33 +0300

    Update project_processor.cpp

--- server/src/project_processor.cpp
+++ server/src/project_processor.cpp
@@ -68,8 +68,8 @@ QJsonDocument RequestHandler::create_project(const QJsonObject &request
     qDebug() << "got for create project";
 
     PrivateUserInfo sender = extract_user_info_from_qjson(request);
-    qDebug() << sender.name;
-    qDebug() << sender.user_role;
+    qDebug() << QString::fromStdString(sender.name);
+    qDebug() << QString::fromStdString(sender.user_role);
 
     std::string project_name =
         request.value("project_name").toString().toStdString();
