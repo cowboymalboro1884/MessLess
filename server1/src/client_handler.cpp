1b05f30e594ae42a4961886e41da95045c52602f
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-06-26 18:22:00 +0300

    Update client_handler.cpp

--- server1/src/client_handler.cpp
+++ server1/src/client_handler.cpp
@@ -30,10 +30,11 @@ void ClientHandler::send_message_to_concrete_user(
         const QJsonDocument &response
 ) {
     qDebug() << 1;
-    if(get_emails()[email]){
+    if (get_emails()[email]) {
         int user_id_to_send = get_emails()[email];
         std::string encrypted_message_text = Encrypting::encrypt(
-                response.object()["message_text"].toString().toStdString(), get_encrypting_keys()[user_id_to_send].PublicKey);
+                response.object()["message_text"].toString().toStdString(),
+                get_encrypting_keys()[user_id_to_send].PublicKey);
         response.object()["message_text"] = QString::fromStdString(encrypted_message_text);
         qDebug() << email;
         get_clients()[user_id_to_send]->write_byte_data(response.toJson());
@@ -46,16 +47,19 @@ void ClientHandler::send_message_to_project_user_list(
 ) {
     for (const auto &obj: users) {
         qDebug() << obj["email"].toString();
-        int socket_id = get_emails()[obj["email"].toString()];
+        if (get_emails()[obj["email"].toString()]) {
+            int socket_id = get_emails()[obj["email"].toString()];
 
 
-        std::string encrypted_message_text = Encrypting::encrypt(
-                message.object()["message_text"].toString().toStdString(), get_encrypting_keys()[socket_id].PublicKey);
-        message.object()["message_text"] = QString::fromStdString(encrypted_message_text);
+            std::string encrypted_message_text = Encrypting::encrypt(
+                    message.object()["message_text"].toString().toStdString(),
+                    get_encrypting_keys()[socket_id].PublicKey);
+            message.object()["message_text"] = QString::fromStdString(encrypted_message_text);
 
-        get_clients()[socket_id]->write_byte_data(
-                message.toJson()
-        );
+            get_clients()[socket_id]->write_byte_data(
+                    message.toJson()
+            );
+        }
     }
     qDebug() << message.object().value("emails_of_recipients").toString();
 }
