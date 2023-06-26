ca6112c464d32d190be1707dd977a98287de2a50
Daniil Neykov <112244451+cowboymalboro1884@users.noreply.github.com>
2023-06-26 17:06:55 +0300

    Update request_handler.hpp

--- server1/include/request_handler.hpp
+++ server1/include/request_handler.hpp
@@ -1,87 +1,358 @@
-#ifndef REQUEST_HANDLER_H
-#define REQUEST_HANDLER_H
+#ifndef RESPONSETEMPLATE_H
+#define RESPONSETEMPLATE_H
 
-#include <QByteArray>
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
-#include <QTcpSocket>
-#include <random>
+#include <string>
+#include <vector>
+#include "responses_types.hpp"
+#include "encrypting.hpp"
 
-#include "database.hpp"
-#include "database_chats.hpp"
-#include "database_project.hpp"
-#include "errors_templates.hpp"
+namespace templates::ResponseTemplate {
 
-using namespace templates::ResponseTemplate;
-using namespace messless;
-
-class RequestHandler {
-    messless::Database *database;
-
-public:
-    RequestHandler(messless::Database *database_);
-
-    [[nodiscard]] QJsonDocument proccess_data(const QByteArray &incoming_data
-    ) const;
-
-    [[nodiscard]] QJsonDocument validate_user(const QJsonObject &request) const;
-    [[nodiscard]] QJsonDocument register_company_with_admin(
-        const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] QJsonDocument add_user_to_company(const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] QJsonDocument create_project(const QJsonObject &request
-    ) const;
-    [[nodiscard]] QJsonDocument get_projects(const QJsonObject &request) const;
-
-    [[nodiscard]] QJsonDocument add_or_delete_user_in_project(
-        const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] QJsonDocument delete_project(const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] QJsonDocument create_task(const QJsonObject &request) const;
-    [[nodiscard]] QJsonDocument get_tasks(const QJsonObject &request) const;
-    [[nodiscard]] QJsonDocument change_task_condition(const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] QJsonDocument delete_task(const QJsonObject &request) const;
-
-    [[nodiscard]] QJsonDocument send_message_to_company(
-        const QJsonObject &request
-    ) const;
-    [[nodiscard]] QJsonDocument send_message_to_project(
-        const QJsonObject &request
-    ) const;
-
-    [[nodiscard]] messless::PrivateUserInfo extract_user_info_from_qjson(
-        const QJsonObject &request
-    ) const noexcept {
-        std::string email = request.value("sender_email").toString().toStdString();
-        std::string password =
-            request.value("sender_password").toString().toStdString();
-        std::string user_role =
-            request.value("sender_user_role").toString().toStdString();
-
-        qDebug() << QString::fromStdString(email);
-        return {email, password, user_role};
-    };
-
-    [[nodiscard]] QJsonArray get_raw_projects_and_tasks(
-        messless::PrivateUserInfo user
-    ) const noexcept;
-
-    [[nodiscard]] QJsonArray get_raw_chat(int company_id, int project_id)
-        const noexcept;
-
-    void get_actual_global_condition_response(
-        SendActualGlobalConditionResponse &response,
-        messless::PrivateUserInfo &user
-    ) const;
+struct UserInfo {
+    QString email;
+    QString password;
+    QString user_role;
 };
 
-#endif  // REQUEST_HANDLER_H
+struct Message {
+    QString name_of_sender;
+    QString surname_of_sender;
+    QString user_role_of_sender;
+
+    QString message_text;
+    QString file_link;
+};
+
+struct AbstractJSONResponse {
+    QString status;
+    QString event_type;
+    RecipientType recipient;
+    QJsonObject main_data;
+
+    AbstractJSONResponse(const QString &event_type_)
+        : event_type(event_type_){};
+
+    AbstractJSONResponse(
+        const QString &event_type_,
+        const QString &status_,
+        RecipientType recipient_
+    )
+        : status(status_), event_type(event_type_), recipient(recipient_){};
+
+    void set_recipient(RecipientType new_recipient_) {
+        recipient = new_recipient_;
+    }
+
+    virtual ~AbstractJSONResponse() = default;
+
+    virtual QJsonDocument to_qjson_document() {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        return QJsonDocument(main_data);
+    }
+};
+
+struct SendActualGlobalConditionResponse : AbstractJSONResponse {
+    QJsonArray projects_with_tasks;
+    QJsonArray company_chat;
+    QJsonArray projects_chats;
+    UserInfo user_info;
+    int company_id;
+
+    SendActualGlobalConditionResponse(const QString &event_type_)
+        : AbstractJSONResponse(event_type_, "success", TO_SENDER){};
+
+    virtual ~SendActualGlobalConditionResponse() = default;
+
+    void set_projects_with_tasks(const QJsonArray &projects_with_tasks_) {
+        projects_with_tasks = std::move(projects_with_tasks_);
+    }
+
+    void set_company_chat(const QJsonArray &company_chat_) {
+        company_chat = std::move(company_chat_);
+    }
+
+    void set_projects_chats(const QJsonArray &projects_chats_) {
+        projects_chats = std::move(projects_chats_);
+    }
+
+    void set_company_id(int company_id_) {
+        company_id = company_id_;
+    }
+
+    void set_user_info(
+        const std::string &email,
+        const std::string &password,
+        const std::string &user_role
+    ) {
+        user_info = UserInfo{
+            QString::fromStdString(email), QString::fromStdString(password),
+            QString::fromStdString(user_role)};
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["email"] = user_info.email;
+        main_data["password"] = user_info.password;
+        main_data["user_role"] = user_info.user_role;
+
+        main_data["projects_with_tasks"] = projects_with_tasks;
+        main_data["company_chat"] = company_chat;
+        main_data["projects_chats"] = projects_chats;
+
+        return QJsonDocument(main_data);
+    }
+};
+
+struct GetTasksOfProjectResponse : AbstractJSONResponse {
+    QJsonArray tasks_of_project;
+    QString project_name;
+
+    GetTasksOfProjectResponse(const std::string &project_name_)
+        : AbstractJSONResponse(
+              response_types[GOT_TASKS_TO_UPDATE],
+              "success",
+              TO_SENDER
+          ),
+          project_name(QString::fromStdString(project_name_)){};
+
+    void set_tasks_of_project(const QJsonArray &tasks) {
+        tasks_of_project = std::move(tasks);
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["project_name"] = project_name;
+        main_data["tasks_of_project"] = tasks_of_project;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct GetProjectsWithUserResponse : AbstractJSONResponse {
+    QJsonArray projects_of_company;
+
+    GetProjectsWithUserResponse()
+        : AbstractJSONResponse(
+              response_types[GOT_PROJECTS_TO_UPDATE],
+              "success",
+              TO_SENDER
+          ){};
+
+    void set_projects_of_company(const QJsonArray &projects) {
+        projects_of_company = std::move(projects);
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["projects_of_company"] = projects_of_company;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct RecievedMessageResponse : AbstractJSONResponse {
+    Message message_to_send;
+    QJsonArray recipients;
+    QString project_name;
+    int company_id;
+
+    RecievedMessageResponse(
+        const std::string &name_of_sender_,
+        const std::string &surname_of_sender_,
+        const std::string &user_role_of_sender_,
+        const std::string &message_text_,
+        const std::string &file_link_,
+        int company_id_
+    )
+        : AbstractJSONResponse(
+              response_types[GOT_MESSAGE_TO_COMPANY_CHAT],
+              "success",
+              TO_COMPANY
+          ),
+          company_id(company_id_) {
+        message_to_send = {
+            QString::fromStdString(name_of_sender_),
+            QString::fromStdString(surname_of_sender_),
+            QString::fromStdString(user_role_of_sender_),
+            QString::fromStdString(message_text_),
+            QString::fromStdString(file_link_)};
+    }
+
+    void change_to_project_chat(
+        const QJsonArray &recipients_,
+        const QString &project_name_
+    ) {
+        response_types[GOT_MESSAGE_TO_PROJECT_CHAT],
+            recipient = TO_PROJECT_USER_LIST;
+        recipients = std::move(recipients_);
+        project_name = project_name_;
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["name_of_sender"] = message_to_send.name_of_sender;
+        main_data["surname_of_sender"] = message_to_send.surname_of_sender;
+        main_data["user_role_of_sender"] = message_to_send.user_role_of_sender;
+        main_data["message_text"] = message_to_send.message_text;
+        main_data["file_link"] = message_to_send.file_link;
+
+        main_data["company_id"] = company_id;
+        main_data["emails_of_recipients"] = recipients;
+        main_data["project_name"] = project_name;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct ProjectUserDeletedOrAdded final : AbstractJSONResponse {
+    QString recipient_email;
+    QString project_name;
+    QString project_bio;
+    int project_id;
+
+    ProjectUserDeletedOrAdded(
+        const std::string &project_name_,
+        const std::string &project_bio_,
+        int project_id_
+    )
+        : AbstractJSONResponse(
+              response_types[NEW_CONDITION_OF_PROJECTS],
+              "success",
+              TO_CONCRETE_USER
+          ),
+          project_name(QString::fromStdString(project_name_)),
+          project_bio(QString::fromStdString(project_bio_)),
+          project_id(project_id_){};
+
+    ProjectUserDeletedOrAdded(const std::string &project_name_)
+        : AbstractJSONResponse(
+              response_types[NEW_CONDITION_OF_PROJECTS],
+              "success",
+              TO_CONCRETE_USER
+          ),
+          project_name(QString::fromStdString(project_name_)){};
+
+    void set_recipient(const QString &recipient_email_) {
+        recipient_email = std::move(recipient_email_);
+    }
+    
+    void set_project_id(int project_id_) {
+        project_id = project_id_;
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["reciever_email"] = recipient_email;
+        main_data["project_name"] = project_name;
+        main_data["project_bio"] = project_bio;
+        main_data["project_id"] = project_id;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct TaskConditionUpdatedSignal : AbstractJSONResponse {
+    QJsonArray recipients;
+    QString project_name;
+    TaskConditionUpdatedSignal(const std::string &project_name_)
+        : AbstractJSONResponse(
+              response_types[NEW_CONDITION_OF_TASKS],
+              "success",
+              TO_PROJECT_USER_LIST
+          ),
+          project_name(QString::fromStdString(project_name_)){};
+
+    void set_recipients(const QJsonArray &recipients_) {
+        recipients = std::move(recipients_);
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["emails_of_recipients"] = recipients;
+        main_data["project_name"] = project_name;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct ProjectIsDeleted final : AbstractJSONResponse {
+    QString project_name;
+    QJsonArray recipients;
+
+    ProjectIsDeleted(const std::string &project_name_)
+        : AbstractJSONResponse(
+              response_types[NEW_CONDITION_OF_PROJECTS],
+              "success",
+              TO_PROJECT_USER_LIST
+          ),
+          project_name(QString::fromStdString(project_name_)){};
+
+    void set_recipients(const QJsonArray &recipients_) {
+        recipients = std::move(recipients_);
+    }
+
+    QJsonDocument to_qjson_document() override {
+        main_data["status"] = status;
+        main_data["type"] = event_type;
+        main_data["recipient"] = recipient;
+
+        main_data["emails_of_recipients"] = recipients;
+        main_data["project_name"] = project_name;
+        return QJsonDocument(main_data);
+    }
+};
+
+struct ProjectCreationIsSuccess final : AbstractJSONResponse {
+    ProjectCreationIsSuccess(const std::string &project_name)
+        : AbstractJSONResponse(
+              response_types[NEW_CONDITION_OF_PROJECTS],
+              "success",
+              TO_SENDER
+          ){};
+};
+
+struct AuthorizationIsSuccess final : SendActualGlobalConditionResponse {
+    AuthorizationIsSuccess()
+        : SendActualGlobalConditionResponse(
+              response_types[GOT_STATUS_OF_AUTHORIZATION]
+          ){};
+};
+
+struct RegistrationCompanyIsSuccess final : SendActualGlobalConditionResponse {
+    RegistrationCompanyIsSuccess()
+        : SendActualGlobalConditionResponse(
+              response_types[GOT_STATUS_OF_REGISTRATION]
+          ){};
+};
+
+struct RegistrationUserIsSuccess final : AbstractJSONResponse {
+    RegistrationUserIsSuccess()
+        : AbstractJSONResponse(
+              response_types[REGISTRATION_IS_SUCCESS],
+              "success",
+              TO_SENDER
+          ){};
+};
+
+}  // namespace templates::ResponseTemplate
+
+#endif  // RESPONSETEMPLATE_H
